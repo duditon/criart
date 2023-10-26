e141cc542d84cec9a019a0bb8d52d182c3e1a999
Eduardo Secondo <59346924+duditon@users.noreply.github.com>
2023-10-26 14:17:12 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,106 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+
+/* menu*/
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+
+    return this;
+  }
+}
+
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+
+mobileNavbar.init();
+
+/*menu*/
+
+window.addEventListener("DOMContentLoaded", () => {
+  const text2 = document.querySelector(".texto2")
+  const imagem = document.querySelector(".imagem")
+  const img = document.querySelector(".imagem img")
+
+  if (!navigator.userAgent.includes("Mobile")) {
+    text2.onmouseover = () => {
+      img.classList.add("imagemHover")
+    }
+    text2.onmouseout = () => {
+      img.classList.remove("imagemHover")
+    }
+  } else {
+    imagem.onclick = () => {
+      img.classList.toggle("imagemHover")
+    }
+  }
+
+})
+
